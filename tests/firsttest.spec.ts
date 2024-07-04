08ae55516a9fee76411d27aadd738f80b42610a6
Pchelachello <aliaksei.pchelnikau1@gmail.com>
2024-07-04 22:23:01 +0300

    AutomationLesson1

---
+++ tests/firsttest.spec.ts
@@ -0,0 +1,14 @@
+import { deepEqual } from "assert";
+import { Methods } from "../src/methods";
+import { notEqualMessage } from "../const/consts";
+
+describe("First test", () => {
+  const numberArray = [1, 3, 2, 4, 9, 6, 7, 0];
+
+  it("Correct work of asc sort", () => {
+    const actualResult = Methods.sortNumberArray(numberArray, "asc");
+    const expectedResult = [0, 1, 2, 3, 4, 6, 7, 9];
+
+    deepEqual(actualResult, expectedResult, notEqualMessage);
+  });
+});
---
+++ tests/testjest.spec.ts
@@ -0,0 +1,11 @@
+describe('Test', ()=>{
+    test.only('Test 1',()=>{
+        expect('string').not.toBe('string')
+    })
+    test('Test 1',()=>{
+        expect('string').not.toBe('string')
+    })
+    test('Test 1',()=>{
+        expect('string').not.toBe('string')
+    })
+})
